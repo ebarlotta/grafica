7a82b79817ae8754b026b8212a15ed6187bbef4b
mariorojaslr <83791643+mariorojaslr@users.noreply.github.com>
2024-09-25 02:25:13 -0300

    MARIO 25-09-2024

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -341,101 +341,110 @@
                                     .gallery-img {
                                         width: 100%;
                                         height: auto;
-                                        /*max-height: 200px; /* Ajusta esta altura máxima según tus necesidades */
-                                        aspect-ratio: 1 / 1; /* Mantiene una proporción 1:1 para ancho y alto */
-                                        /* object-fit: cover; /* Asegura que la imagen cubra el contenedor */
-                                        object-fit: contain;
+                                        max-height: 200px; /* Ajusta esta altura máxima según tus necesidades */
+                                        object-fit: contain; /* Asegura que la imagen se ajuste dentro del contenedor sin recortarse */
+                                        border: 2px solid #000; /* Agrega un borde negro de 2px */
+                                        box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.3); /* Agrega una sombra desplazada */
+                                        padding: 5px; /* Espacio entre la imagen y el borde */
+                                        transition: transform 0.3s ease-in-out; /* Añade una transición suave */
                                     }
+
+                                    .gallery-img:hover {
+                                        transform: scale(1.05); /* Aumenta ligeramente el tamaño de la imagen */
+                                    }
+
                                 </style>
 
 
 
+
+
                                 <div class="container mt-5">
-                                    <div class="row">
+                                    <div class="row ms-3 me-3">
                                         <!-- Fila 1 -->
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/Libros 01.jpg') }}" alt="Imagen 1" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/Libros 02.jpg') }}" alt="Imagen 2" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/Libros 03.jpg') }}" alt="Imagen 3" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/Libros 04.jpg') }}" alt="Imagen 4" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/Libros 05.jpg') }}" alt="Imagen 5" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/Libros 06.jpg') }}" alt="Imagen 6" class="gallery-img img-fluid mb-3">
                                         </div>
                                         <!-- Fila 2 -->
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/remeras 01.jpg') }}" alt="Imagen 7" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/remeras 02.jpg') }}" alt="Imagen 8" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/remeras 03.jpg') }}" alt="Imagen 9" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/remeras 04.jpg') }}" alt="Imagen 10" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/remeras 05.jpg') }}" alt="Imagen 11" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/mercha 01.jpg') }}" alt="Imagen 12" class="gallery-img img-fluid mb-3">
                                         </div>
                                         <!-- Fila 3 -->
 
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/estampado 01.jpg') }}" alt="Imagen 14" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/estampado 02.jpg') }}" alt="Imagen 15" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/estampado 03.jpg') }}" alt="Imagen 16" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/estampado 04.jpg') }}" alt="Imagen 17" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/estampado 05.jpg') }}" alt="Imagen 18" class="gallery-img img-fluid mb-3">
                                         </div>
                                         <!-- Fila 4 -->
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/estampado 06.jpg') }}" alt="Imagen 19" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/estampado 07.jpg') }}" alt="Imagen 20" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/cajas 01.jpg') }}" alt="Imagen 21" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/cajas 02.jpg') }}" alt="Imagen 22" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/cajas 03.jpg') }}" alt="Imagen 23" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/tarjetas 01.jpg') }}" alt="Imagen 24" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/ploter 01.jpg') }}" alt="Imagen 25" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/ploter 02.jpg') }}" alt="Imagen 26" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/rotulacion 01.jpg') }}" alt="Imagen 27" class="gallery-img img-fluid mb-3">
                                         </div>
-                                        <div class="col-6 col-md-2">
+                                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 mb-3">
                                             <img src="{{ asset('fotos/etiquetas 01.jpg') }}" alt="Imagen 28" class="gallery-img img-fluid mb-3">
                                         </div>
 
